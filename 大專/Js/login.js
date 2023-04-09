c13742dd1a49b0ba31555e09e3d1ad2bcbda8e24
pahasohic55 <asdasd122333@yahoo.com.tw>
2023-04-09 19:45:21 +0800

    前端網頁v3

---
+++ 大專/Js/login.js
@@ -0,0 +1,11 @@
+const containerlogin = document.querySelector('.container-login');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=>{
+    containerlogin.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+    containerlogin.classList.remove('active');
+});
