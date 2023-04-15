6dd6c1c29c8a30f8c9e6a04b4a620edaadb72d0d
pahasohic55 <asdasd122333@yahoo.com.tw>
2023-04-15 17:34:25 +0800

    前端網頁v9

--- FinalProWeb/src/main/webapp/Js/sidebar.js
+++ FinalProWeb/src/main/webapp/Js/sidebar.js
@@ -26,3 +26,25 @@ $(document).ready(function () {
           $('#wrapper').toggleClass('toggled');
     });  
   });
+  
+// 會員頭像
+  $(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
